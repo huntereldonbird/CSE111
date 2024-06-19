7707662ceb641de4508df87ddd574c2fb5aaf069
Vitormscastro <145720977+Vitormscastro@users.noreply.github.com>
2024-06-19 17:46:59 -0300

    Add files via upload

---
+++ compute_surface_area.py
@@ -0,0 +1,42 @@
+import math
+
+def main():
+    names = ["#1 Picnic", "#1 Tall", "#2",	"#2.5", "#3 Cylinder", "#5", "#6Z", "#8Z short", "#10", "#211", "#300", "#303"]
+    radius = [6.83, 7.78, 8.73, 10.32, 10.79, 13.02, 5.40, 6.83, 15.72, 6.83, 7.62, 8.10]
+    heights = [10.16, 11.91, 11.59, 11.91, 17.78, 14.29, 8.89, 7.62, 17.78, 12.38, 11.27, 11.11]
+    costs = [0.28, 0.43, 0.45, 0.61, 0.86, 0.83, 0.22, 0.26, 1.53, 0.34, 0.38, 0.42]
+
+    max_value = None
+
+    for i in range(12):
+        volume = compute_volume(radius[i], heights[i])
+        surface_area = compute_surface_area(radius[i], heights[i])
+        storage_efficiency = compute_storage_efficiency(volume, surface_area)
+        cost = compute_cost_efficiency(volume, costs[i])
+        print(f"{names[i]} {storage_efficiency:.2f} ${cost:.2f}")
+
+        if max_value != None:
+            pass
+        else:
+            max_value = costs[i]
+
+    print(f"{names[int(max_value)]} ${compute_cost_efficiency(compute_volume(radius[int(max_value)], heights[int(max_value)]), costs[max_value])}")
+
+
+def compute_volume(radius, height):
+    volume = math.pi * (radius * radius) * height
+    return volume
+
+def compute_surface_area(radius, height):
+    surface_area = 2 * math.pi * radius * (radius + height)
+    return surface_area
+
+def compute_storage_efficiency(volume, surface_area):
+    storage_efficiency = volume / surface_area
+    return storage_efficiency
+
+def compute_cost_efficiency(volume, cost):
+    cost_efficiency = volume / cost
+    return cost_efficiency
+
+main()
