818b43171d592a24d31deede03661838a6df469c
huntereldonbird <56803340+EldonBird@users.noreply.github.com>
2024-07-19 16:33:42 -0600

    kjiuh

--- Game_Grapher.py
+++ Game_Grapher.py
@@ -1,12 +1,34 @@
 import pandas as pd
 
-def GetWinRate(df):
+def GetWinRate(games, wins):
     pass
 
 
+
+def GetPlayerInfo(df, playerName):
+    Wins = 0
+    Games = 0
+
+    for x in range(0, df.shape[1]):
+        cell = df.iloc[4, x]
+
+        if((cell == "Rebel Win") or (cell == "Empire Win") or (cell == "Const Win")):
+            print("WINNER")
+            Wins += 1
+            Games += 1
+        if((cell == "Rebel Loss") or (cell == "Empire Loss") or (cell == "Const Loss")):
+            print("LOSER")
+            Games += 1
+
+    return Wins, Games
+
+
 def main():
-    file = pd.read_excel('Score.xlsx')
-    file.head()
+    df = pd.read_csv("Score.csv")
+
+    wins, loss = GetPlayerInfo(df, "Eldon")
+
+    print(f"Wins: {wins}, Loss: {loss}")
 
 
 
