a8b42ee149daea0826f166d1b99ed548d48de1e3
huntereldonbird <56803340+EldonBird@users.noreply.github.com>
2024-07-23 14:47:49 -0600

    jiudhauiwd

--- Game_Grapher.py
+++ Game_Grapher.py
@@ -1,4 +1,46 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+def GraphFunction(df, playerName):
+
+    elements = []
+    empty = []
+    amount = 0
+    wins = 0
+
+    row_index = df.index[df['Players'] == playerName].tolist()
+
+    for x in range(0, df.shape[1]):
+
+        cell = df.iloc[row_index[0], x]
+        if((cell == "Rebel Win") or (cell == "Empire Win") or (cell == "Const Win")):
+            amount += 1
+            wins += 1
+        if((cell == "Rebel Loss") or (cell == "Empire Loss") or (cell == "Const Loss")):
+            amount += 1
+        
+        if(amount != 0):
+            temp = GetWinRate(amount, wins)
+            elements.append(temp)
+
+    for t in range(0, len(elements)):
+        empty.append(t)
+
+
+    plt.plot(empty, elements)
+
+    plt.ylabel = "Win %"
+    plt.xlabel = "# of Games"
+
+    plt.xlim([0, len(empty)])
+    plt.ylim([-1, 101])
+
+    plt.show()
+
+    return elements
+
+
+
 
 def GetWinRate(games, wins):
     
@@ -17,20 +59,25 @@ def GetPlayerInfo(df, playerName):
     Wins = 0
     Games = 0
 
-    row_index = df.index[df['Players'] == playerName].tolist()
+    try:
 
-    for x in range(0, df.shape[1]):
-        cell = df.iloc[row_index[0], x]
+        row_index = df.index[df['Players'] == playerName].tolist()
 
-        if((cell == "Rebel Win") or (cell == "Empire Win") or (cell == "Const Win")):
-            print("WINNER")
-            Wins += 1
-            Games += 1
-        if((cell == "Rebel Loss") or (cell == "Empire Loss") or (cell == "Const Loss")):
-            print("LOSER")
-            Games += 1
+        for x in range(0, df.shape[1]):
+            cell = df.iloc[row_index[0], x]
 
-    return Wins, Games
+            if((cell == "Rebel Win") or (cell == "Empire Win") or (cell == "Const Win")):
+                print("WINNER")
+                Wins += 1
+                Games += 1
+            if((cell == "Rebel Loss") or (cell == "Empire Loss") or (cell == "Const Loss")):
+                print("LOSER")
+                Games += 1
+
+        return Wins, Games
+    except:
+        print("NOT FOUND")
+    
 
 
 def main():
@@ -44,6 +91,8 @@ def main():
     percent = GetWinRate(games, wins)
 
     print(f"Wins: {wins}, Games: {games} with a win% of, {percent}%")
+
+    GraphFunction(df, playerIn)
 
 
 
--- Game_Grapher_Test.py
+++ Game_Grapher_Test.py
@@ -6,16 +6,77 @@ import pandas as pd
 def test_GetWinRate():
     assert Game_Grapher.GetWinRate(10, 5) == 50, \
         "FAILED"
+
+    assert Game_Grapher.GetWinRate(100, 20) == 20, \
+        "FAILED"
     
 
 def test_GetPlayerInfo():
 
-    file = pd.read_csv("Score.csv")\
+    file = pd.read_csv("Score.csv")
 
     wins, games = Game_Grapher.GetPlayerInfo(file, "Maddy")
 
     assert games == 5, \
         "FAILED"
+
+    wins, games = Game_Grapher.GetPlayerInfo(file, "Eldon")
+    
+    assert games == 7, \
+        "FAILED"
+
+def test_Graph():
+    
+    df = pd.read_csv("Score.csv")
+
+    elements1 = []
+    amount = 0
+    wins = 0
+
+    row_index = df.index[df['Players'] == "Eldon"].tolist()
+
+    for x in range(0, df.shape[1]):
+
+        cell = df.iloc[row_index[0], x]
+        if((cell == "Rebel Win") or (cell == "Empire Win") or (cell == "Const Win")):
+            amount += 1
+            wins += 1
+        if((cell == "Rebel Loss") or (cell == "Empire Loss") or (cell == "Const Loss")):
+            amount += 1
+        
+        if(amount != 0):
+            temp = Game_Grapher.GetWinRate(amount, wins)
+            elements1.append(temp)
+
+
+    assert Game_Grapher.GraphFunction(df, "Eldon") == elements1, \
+        "FAILED THE GRAPH"
+
+    elements2 = []
+    amount2 = 0
+    wins2 = 0
+
+    row_index2 = df.index[df['Players'] == "Maddy"].tolist()
+
+    for x in range(0, df.shape[1]):
+
+        cell = df.iloc[row_index2[0], x]
+        if((cell == "Rebel Win") or (cell == "Empire Win") or (cell == "Const Win")):
+            amount2 += 1
+            wins2 += 1
+        if((cell == "Rebel Loss") or (cell == "Empire Loss") or (cell == "Const Loss")):
+            amount2 += 1
+        
+        if(amount2 != 0):
+            temp = Game_Grapher.GetWinRate(amount2, wins2)
+            elements2.append(temp)
+
+
+    assert Game_Grapher.GraphFunction(df, "Maddy") == elements2, \
+        "FAILED THE GRAPH Test for MADDY"
+
+
+
 
 
 
